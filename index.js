18a2c6f9c423f4dcc1f84c38e4a2a5e333f1b313
Christopher Harris <56790192+decode-harris@users.noreply.github.com>
2020-12-15 17:26:38 +1100

    refactored css
    removed unnecessary :nth-child element properties & created global panel styles

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 
+const page = document.querySelector('.page');
+
+let bk1 = 'img/launch.jpg';
+let bk2 = 'img/travel.jpg';
+
+let launch = document.querySelector('#launch');
+let travel = document.querySelector('#travel');
+
+launch.addEventListener('click', ()=> {
+
+    if (page.style.background === 'var(--slateblue)' || page.style.backgroundImage === 'url(img/launch.jpg)') {
+        page.style.backgroundImage = 'url(' + bk1 + ')';
+    }
+    else {
+        page.style.background = 'var(--slateblue)';
+    }
+
+});
+
+travel.addEventListener('click', ()=> {
+
+    if (page.style.background === 'var(--slateblue)' || page.style.backgroundImage === 'url(img/travel.jpg)') {
+        page.style.backgroundImage = 'url(' + bk2 + ')';
+    }
+    else {
+        page.style.background = 'var(--slateblue)';
+    }
+
+    
+
+});
 
 
